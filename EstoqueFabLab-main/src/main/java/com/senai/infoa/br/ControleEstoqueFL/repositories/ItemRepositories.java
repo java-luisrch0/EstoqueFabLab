fab3f6bfbdb8e694e73ccd34c5c04912ab8e008c
GabrielQuinelato <gabrielpontes1316@gmail.com>
2025-11-19 10:44:58 -0300

    Delete EstoqueFabLab-main directory

--- EstoqueFabLab-main/src/main/java/com/senai/infoa/br/ControleEstoqueFL/repositories/ItemRepositories.java
+++
@@ -1,9 +0,0 @@
-package com.senai.infoa.br.ControleEstoqueFL.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.senai.infoa.br.ControleEstoqueFL.models.Item;
-
-public interface ItemRepositories extends JpaRepository<Item, Integer> {
-    
-}
