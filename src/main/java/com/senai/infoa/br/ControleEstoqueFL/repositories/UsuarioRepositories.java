d39a0739aa71f2966b32e301bccd7b14ad017e7c
luis.rch0 <luiseduardoalmeida2803@gmail.com>
2025-11-22 21:45:13 -0300

    update

--- src/main/java/com/senai/infoa/br/ControleEstoqueFL/repositories/UsuarioRepositories.java
+++ src/main/java/com/senai/infoa/br/ControleEstoqueFL/repositories/UsuarioRepositories.java
@@ -1,12 +1,14 @@
 package com.senai.infoa.br.ControleEstoqueFL.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import com.senai.infoa.br.ControleEstoqueFL.models.Usuario;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import com.senai.infoa.br.ControleEstoqueFL.models.Usuario;
 
 @Repository
 public interface UsuarioRepositories extends JpaRepository<Usuario, Integer> {
-    @Query(value="select * from usuario where email = !email and senha = !senha and funcao = !funcao", nativeQuery = true)
-    public Usuario findByUsuario(String email, String senha, String funcao);
+    @Query(value="select * from usuario where email = email and senha = senha and funcao = funcao", nativeQuery = true)
+    public Usuario findByUsuario(@Param("email") String email,@Param("senha") String senha,@Param("funcao") String funcao);
 }
